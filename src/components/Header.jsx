ef927a01e569e0be9be82fdcb86395841c3f6e41
Michael Khokhlinov <P1304110@ness-tech.co.il>
2020-08-16 16:28:05 +0300

    step 1 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT_tEaTPW-NZBrGioXYxbX9lYLMO39Xcml0Gw&usqp=CAU" />
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2cGUqGnCYT6DxUTYNd8hqbRLcDCZ9c6TsEw&usqp=CAU" ></img>
+        </div>
+        <div>
+            ava + description
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTgolBdeaXdt7hZ4G28YiA8shOCg4jkBg08uA&usqp=CAU"></img>
+        </div>
+
+        <div>
+            my posts
+         <div>
+                new post
+        </div>
+        </div>
+        <div>
+            <div>
+                post 1
+        </div>
+            <div>
+                post 2
+        </div>
+        </div>
+    </div>
+}
+
+export default Profile
