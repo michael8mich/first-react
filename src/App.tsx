f1005a192245d27fe7bfb8235c31ce2577daf13a
michael8mich <mich_hoh@yahoo.com>
2021-10-25 08:56:14 +0300

    UTA(v-1.0.0)

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ const App:FC = () => {
       </Layout.Content>
       <Layout.Footer style={{height:'10px'}}>
         <div style={{fontSize:'8px'}}>
-        UTA System(Users Tickets Assets) Michael Khokhlinov Co 2021
+        UTA System(Users Tickets Assets) v(1.0.0) Michael Khokhlinov Co 2021
         </div>
       </Layout.Footer>
     </Layout>
